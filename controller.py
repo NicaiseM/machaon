08f2d56d635fad0e10550e8b5564feee0b8d5160
NicaiseM <nicaise@rambler.ru>
2026-01-21 20:44:34 +0300

    Добавлена обработка добровольных и назначенных отсутствий.

--- controller.py
+++ controller.py
@@ -8,6 +8,7 @@
 # Standard Library
 import math
 import json
+import random
 from pathlib import Path
 
 # Third-party Libraries
@@ -23,20 +24,23 @@ class Controller():
 
     def __init__(self, cfg, players):
         self.cfg = cfg
-        self.players = players
+        self.tt = players
+        self.players_count = self.tt.shape[0]
+        self.create_tournament_table()
 
-    def simulate_tournament(self):
+    def create_tournament_table(self):
         bands = self.create_bands()
         initial_mm_points = self.calc_initial_mm_points(bands)
-        print(bands)
-        print(initial_mm_points)
+        self.add_initial_mm_points_to_table(bands, initial_mm_points)
 
     def create_bands(self):
-        top_band = players[players['rating'] >= self.cfg['bar']].shape[0]
-        other_players = players.shape[0] - top_band
+        top_band = self.tt[self.tt['rating'] >= self.cfg['bar']].shape[0]
+        other_players = self.players_count - top_band
+        # Сейчас распределяем по оставшимся группам ММ игроков равномерно.
+        # Оставшиеся игроки распределяются в последнюю группу ММ.
         players_in_band = int(other_players/(self.cfg['bands'] - 1))
         bands = [top_band] + (self.cfg['bands'] - 2)*[players_in_band]
-        bands += [players.shape[0] - sum(bands)]
+        bands += [self.players_count - sum(bands)]
         return bands
 
     def calc_initial_mm_points(self, bands):
@@ -51,9 +55,34 @@ class Controller():
         mm_points = [mm_point + max_points for mm_point in mm_points]
         return mm_points
 
+    def add_initial_mm_points_to_table(self, bands, mm_points):
+        mm0_tmp = [band*[mm_point] for band, mm_point in zip(bands, mm_points)]
+        mm0 = [mm_point for band in mm0_tmp for mm_point in band]
+        self.tt['mm0'] = mm0
+        self.tt['mm'] = mm0
+        self.tt['buchholz'] = self.players_count*[0]
+        self.tt['berger'] = self.players_count*[0]
+
+    def simulate_tournament(self):
+        print(self.tt)
+
+    def simulate_round(self):
+        # Сортируем по очкам ММ, к-м Бухгольца, Бергера и рейтингу
+        self.tt = self.tt.sort_values(
+            by=['mm', 'buchholz', 'berger', 'rating'])
+        # Случайно определяем добровольно отсутствующих.
+        volyntary_byes = [
+            random.random() < 0.05 for i in range(self.players_count)]
+        volyntary_byes_count = sum(volyntary_byes)
+        forced_byes = self.players_count*[False]
+        if (self.players_count - volyntary_byes_count)%2:
+            # Назначаем отсутствующим последнего присутствующего игрока.
+            forced_bye_idx = volyntary_byes.rindex(False)
+            forced_byes[forced_bye_idx] = True
+
     def calc_probability_by_elo(self, players_pair):
-        R0 = self.players.loc[players_pair[0], 'rating']
-        R1 = self.players.loc[players_pair[1], 'rating']
+        R0 = self.tt.loc[players_pair[0], 'rating']
+        R1 = self.tt.loc[players_pair[1], 'rating']
         E0 = 1/(1 + 10**((R1-R0)/400))
         E1 = 1 - E0
         return [E0, E1]
@@ -83,7 +112,6 @@ if __name__ == '__main__':
         'rounds': 5,
         'bar': 2300,
         'bands': 8,
-        'players_in_band': 7,
         'D': 0.75,
         }
     controller = Controller(cfg, players)
